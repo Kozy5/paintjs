a22cb052d7ebd78bb56562608680361877cba225
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2022-01-24 12:02:49 +0900

    event-function

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const canvas = document.querySelector("#jsCanvas");
+
+let painting = false;
+function stopPainting() {
+  painting = false;
+}
+function handleMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+}
+
+function onMouseDown(event) {
+  painting = true;
+}
+function onMouseUp(event) {
+  stopPainting();
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", handleMouseMove);
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
